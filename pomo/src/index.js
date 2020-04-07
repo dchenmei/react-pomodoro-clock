fd1e5212e2854ea64ba6c36167c7bc4d9fae3b41
dchenmei <dchenmei@users.noreply.github.com>
2020-04-07 10:33:22 -0400

    Added react-redux library

--- pomo/src/index.js
+++ pomo/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/PomoApp';
+import App from './components/PomoApp';
 import './index.css';
 
 ReactDOM.render(
